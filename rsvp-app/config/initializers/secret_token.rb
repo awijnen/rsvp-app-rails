827b6464d5ed825167f198df8a142cbe558feedc
Anthony Wijnen <anthony.wijnen@gmail.com>
2013-02-28 15:30:50 -0500

    new rails app first push

---
+++ rsvp-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RsvpApp::Application.config.secret_token = '997d687e679c8c7833ef9ec4705eea74f14015bb6d8f310c038ae1471168061d677ed632e0af6008ce5d19459a1003947216a32cec75f4c7e5d713d7568651f5'
